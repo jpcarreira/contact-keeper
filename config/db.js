62145f2e1286dae42aa472407493624cc2fe1bc0
Joao Carreira <joaocarreira@me.com>
2019-09-03 16:07:39 +0100

    refactor: connect to Mongo DB using async / await

--- config/db.js
+++ config/db.js
@@ -2,18 +2,16 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: true
-    })
-    .then(() => console.log('MongoDB connected!'))
-    .catch(err => {
-      console.log(`MongoDB connection error: ${err.message}`);
-      process.exit(1);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
     });
+    console.log('Connected to DB');
+  } catch (err) {
+    console.log(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
