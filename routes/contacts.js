62e8d584ade928776c98e3b5f990a7327729655b
Joao Carreira <joaocarreira@me.com>
2019-09-04 17:30:58 +0100

    feat: implemented endpoint to edit and delete existing contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -62,19 +62,67 @@ router.post(
 // @route     PUT /api/contacts/:id
 // @desc      Updates a given contact
 // @access    Private
-router.put('/:id', (req, res) => {
-  res.send({
-    msg: 'Updates a contact'
-  });
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  const contactFields = {};
+  if (name) {
+    contactFields.name = name;
+  }
+  if (email) {
+    contactFields.email = email;
+  }
+  if (phone) {
+    contactFields.phone = phone;
+  }
+  if (type) {
+    contactFields.type = type;
+  }
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route     DELETE /api/contacts/:id
 // @desc      Deletes a given contact
 // @access    Private
-router.delete('/:id', (req, res) => {
-  res.send({
-    msg: 'Deletes a contact'
-  });
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Contact removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
