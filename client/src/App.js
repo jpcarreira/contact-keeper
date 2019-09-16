75a1665f4f6aaef1299b3e9b1befa27e0e0d48a9
Joao Carreira <joaocarreira@me.com>
2019-09-16 07:55:57 +0100

    chore: removed About page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Navbar from './components/layout/Navbar';
 import Alerts from './components/layout/Alerts';
 
 import Home from './components/pages/Home';
-import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
@@ -34,7 +33,6 @@ const App = () => {
                 <Alerts />
                 <Switch>
                   <PrivateRoute exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
                 </Switch>
