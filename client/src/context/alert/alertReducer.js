0916d6b3bd33ce618cb2f2da2ac2ac859167e183
Joao Carreira <joaocarreira@me.com>
2019-09-18 22:29:12 +0100

    fix: multiple identical alerts showing up

--- client/src/context/alert/alertReducer.js
+++ client/src/context/alert/alertReducer.js
@@ -3,7 +3,9 @@ import { SET_ALERT, REMOVE_ALERT } from '../types';
 export default (state, action) => {
   switch (action.type) {
     case SET_ALERT:
-      return [...state, action.payload];
+      const exists = state.some(alert => alert.msg === action.payload.msg);
+      return exists ? state : [...state, action.payload];
+
     case REMOVE_ALERT:
       return state.filter(alert => alert.id !== action.payload);
     default:
